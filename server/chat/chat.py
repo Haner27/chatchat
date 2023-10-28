13e1b2b84982f02e4992d14bc58f4b93bd34a01f
sikai zhang <sikai@biomap.com>
2023-10-28 20:22:59 +0800

    fix

--- server/chat/chat.py
+++ server/chat/chat.py
@@ -12,27 +12,39 @@ from server.chat.utils import History
 from server.utils import get_prompt_template
 
 
-async def chat(query: str = Body(..., description="用户输入", examples=["恼羞成怒"]),
-                history: List[History] = Body([],
-                                       description="历史对话",
-                                       examples=[[
-                                           {"role": "user", "content": "我们来玩成语接龙，我先来，生龙活虎"},
-                                           {"role": "assistant", "content": "虎头虎脑"}]]
-                                       ),
-                stream: bool = Body(False, description="流式输出"),
-                model_name: str = Body(LLM_MODEL, description="LLM 模型名称。"),
-                temperature: float = Body(TEMPERATURE, description="LLM 采样温度", ge=0.0, le=1.0),
-                max_tokens: int = Body(None, description="限制LLM生成Token数量，默认None代表模型最大值"),
-                # top_p: float = Body(TOP_P, description="LLM 核采样。勿与temperature同时设置", gt=0.0, lt=1.0),
-                prompt_name: str = Body("default", description="使用的prompt模板名称(在configs/prompt_config.py中配置)"),
-         ):
+async def chat(
+    query: str = Body(..., description="用户输入", examples=["恼羞成怒"]),
+    history: List[History] = Body(
+        [],
+        description="历史对话",
+        examples=[
+            [
+                {"role": "user", "content": "我们来玩成语接龙，我先来，生龙活虎"},
+                {"role": "assistant", "content": "虎头虎脑"},
+            ]
+        ],
+    ),
+    stream: bool = Body(False, description="流式输出"),
+    model_name: str = Body(LLM_MODEL, description="LLM 模型名称。"),
+    temperature: float = Body(TEMPERATURE, description="LLM 采样温度", ge=0.0, le=1.0),
+    max_tokens: int = Body(None, description="限制LLM生成Token数量，默认None代表模型最大值"),
+    # top_p: float = Body(TOP_P, description="LLM 核采样。勿与temperature同时设置", gt=0.0, lt=1.0),
+    prompt_name: str = Body(
+        "default", description="使用的prompt模板名称(在configs/prompt_config.py中配置)"
+    ),
+):
+    # save all args into a json file
+    with open("args.json", "w") as f:
+        json.dump(locals(), f)
+
     history = [History.from_data(h) for h in history]
 
-    async def chat_iterator(query: str,
-                            history: List[History] = [],
-                            model_name: str = LLM_MODEL,
-                            prompt_name: str = prompt_name,
-                            ) -> AsyncIterable[str]:
+    async def chat_iterator(
+        query: str,
+        history: List[History] = [],
+        model_name: str = LLM_MODEL,
+        prompt_name: str = prompt_name,
+    ) -> AsyncIterable[str]:
         callback = AsyncIteratorCallbackHandler()
         model = get_ChatOpenAI(
             model_name=model_name,
@@ -44,13 +56,13 @@ async def chat(query: str = Body(..., description="用户输入", examples=["恼
         prompt_template = get_prompt_template("llm_chat", prompt_name)
         input_msg = History(role="user", content=prompt_template).to_msg_template(False)
         chat_prompt = ChatPromptTemplate.from_messages(
-            [i.to_msg_template() for i in history] + [input_msg])
+            [i.to_msg_template() for i in history] + [input_msg]
+        )
         chain = LLMChain(prompt=chat_prompt, llm=model)
 
         # Begin a task that runs in the background.
-        task = asyncio.create_task(wrap_done(
-            chain.acall({"input": query}),
-            callback.done),
+        task = asyncio.create_task(
+            wrap_done(chain.acall({"input": query}), callback.done),
         )
 
         if stream:
@@ -65,8 +77,21 @@ async def chat(query: str = Body(..., description="用户输入", examples=["恼
 
         await task
 
-    return StreamingResponse(chat_iterator(query=query,
-                                           history=history,
-                                           model_name=model_name,
-                                           prompt_name=prompt_name),
-                             media_type="text/event-stream")
+    return StreamingResponse(
+        chat_iterator(
+            query=query, history=history, model_name=model_name, prompt_name=prompt_name
+        ),
+        media_type="text/event-stream",
+    )
+
+
+# read args from json file and test chat
+def test_chat():
+    with open("args.json", "r") as f:
+        args = json.load(f)
+    for i in chat(**args):
+        print(i)
+
+
+if __name__ == "__main__":
+    test_chat()
