291863b2d3e01da102a68e3c5530273cb4e129fb
sikai zhang <sikai@biomap.com>
2023-10-28 20:35:28 +0800

    fix

--- server/chat/chat.py
+++ server/chat/chat.py
@@ -45,7 +45,7 @@ async def chat(
         prompt_name=prompt_name,
     )
     # save all args into a json file
-    with open("args.pkl", "w") as f:
+    with open("args.pkl", "wb") as f:
         pickle.dump(args, f)
 
     history = [History.from_data(h) for h in history]
