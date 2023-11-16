59770d2e03a98343ea1eef4f2c80ac9d2b48401b
sikai zhang <sikai@biomap.com>
2023-11-16 22:21:41 +0800

    fix

--- baidu_tongji.js
+++ baidu_tongji.js
@@ -1,14 +1,9 @@
-<html>
-  <head>
-    <script>
-    var _hmt = _hmt || [];
+<script>
+var _hmt = _hmt || [];
 (function() {
-    var hm = document.createElement("script");
-    hm.src = "https://hm.baidu.com/hm.js?1b9d6b726058df31cf82d9d2512d4e4e";
-    var s = document.getElementsByTagName("script")[0];
-    s.parentNode.insertBefore(hm, s);
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?1b9d6b726058df31cf82d9d2512d4e4e";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
 })();
 </script>
-</head>
-  <body></body>
-</html>
